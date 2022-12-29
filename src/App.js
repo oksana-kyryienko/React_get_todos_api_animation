16ddab7b592ae472b0ff77928c960dfda3387d6f
Oksana Kyryienko <kseniyals1609@gmail.com>
2022-12-30 01:47:16 +0200

    navbar and page not found were added

--- src/App.js
+++ src/App.js
@@ -1,41 +1,22 @@
-// import React, { useEffect, useState } from "react";
 import '../src/styles/App.css';
-// import { ClassCounter } from "./components/ClassCounter";
-// import { Counter } from "./components/Counter";
-// import { Input } from "./components/Input";
-// import { PostList } from "./components/PostList";
-// import PostForm from "./components/PostForm";
-// import PostFilter from "./components/PostFilter";
-// import MyModal from "./components/UI/MyModal/MyModal";
-// import MyBytton from "./components/UI/button/MyButton";
-// import { usePosts } from "./hooks/usePosts";
-// import PostService from "./API/PostService";
-// import Loader from "./components/UI/loader/Loader";
-// import { useFetching } from "./hooks/UseFetching";
-// import { getPageCount } from "./components/utils/pages";
-// import Pagination from "./components/UI/pagination/Pagination";
 import React from 'react';
-import { Link, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import About from './pages/About/About';
 import Posts from './pages/Posts/Posts';
+import Navbar from './components/UI/Navbar/Navbar';
+import PageNotFound from './components/UI/PageNotFound/PageNotFound';
 
 function App() {
   return (
   <>
-      <div className='navbar'>
-        <div className="navbar__links">
-          <Link className='link' to='/about'>About website</Link>
-          <Link className='link' to='/posts'>Posts</Link>
-        </div>
-      </div>
+    <Navbar />
     <Routes>
-    
       <Route path="/about" element={<About />} />
       <Route path="/posts" element={<Posts />} />
+      <Route path="*" element={<PageNotFound />} />
     </Routes>
   </>
   )
-
 }
 
 export default App;
